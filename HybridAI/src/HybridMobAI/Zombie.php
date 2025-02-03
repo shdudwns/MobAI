949e9728134f23f44e3799133d6ede3bea5a1649
jun04 <jun040329@gmail.com>
2025-02-03 17:34:05 +0900

    Update Zombie.php

--- HybridAI/src/HybridMobAI/Zombie.php
+++ HybridAI/src/HybridMobAI/Zombie.php
@@ -3,13 +3,24 @@
 namespace HybridMobAI;
 
 use pocketmine\entity\Monster;
-use pocketmine\entity\Entity;
 use pocketmine\nbt\tag\CompoundTag;
+use pocketmine\world\World;
 
 class Zombie extends Monster {
-    public const NETWORK_ID = Entity::ZOMBIE;
 
-    public function getName(): string {
-        return "Zombie";
+    public function __construct(World $world, CompoundTag $nbt) {
+        parent::__construct($world, $nbt);
+        $this->getLogger()->info("좀비 생성 완료: " . $this->getName());
+    }
+
+    public function onSpawn(): void {
+        parent::onSpawn();
+        $this->getLogger()->info("좀비 스폰 완료: " . $this->getName());
+    }
+
+    public function onUpdate(int $currentTick): bool {
+        // 좀비의 업데이트 로직 (이동, 공격 등)
+        $this->getLogger()->info("좀비 업데이트 중: " . $this->getName());
+        return parent::onUpdate($currentTick);
     }
 }
