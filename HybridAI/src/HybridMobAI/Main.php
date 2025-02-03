7839ce1d90eb566596d7a21942953cf7dc4a4add
jun04 <jun040329@gmail.com>
2025-02-03 19:57:54 +0900

    Update Main.php

--- HybridAI/src/HybridMobAI/Main.php
+++ HybridAI/src/HybridMobAI/Main.php
@@ -96,7 +96,7 @@ class Main extends PluginBase implements Listener {
             ]));
 
         // 좀비 인스턴스 생성
-        $zombie = EntityFactory::getInstance()->spawnFromNBT($nbt, $world);
+        $zombie = EntityFactory::getInstance()->create(Zombie::class, $world, $nbt);
         if ($zombie !== null) {
             $zombie->spawnToAll();
             $this->getLogger()->info("좀비 스폰 완료");
