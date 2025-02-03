f3c31a8c32a4f61908f8477f2c90d556cb6d9656
jun04 <jun040329@gmail.com>
2025-02-03 19:31:12 +0900

    Update Zombie.php

--- HybridAI/src/HybridMobAI/Zombie.php
+++ HybridAI/src/HybridMobAI/Zombie.php
@@ -5,23 +5,21 @@ namespace HybridMobAI;
 use pocketmine\entity\Monster;
 use pocketmine\nbt\tag\CompoundTag;
 use pocketmine\world\World;
+use pocketmine\entity\Location;
 
 class Zombie extends Monster {
 
-    public function __construct(World $world, CompoundTag $nbt) {
-        parent::__construct($world, $nbt);
-        $this->getLogger()->info("좀비 생성 완료: " . $this->getName());
+    public function __construct(Location $location, CompoundTag $nbt) {
+        parent::__construct($location, $nbt);
     }
 
     public function onSpawn(): void {
         parent::onSpawn();
-        $this->getLogger()->info("좀비 스폰 완료: " . $this->getName());
+        $this->getServer()->getLogger()->info("좀비 스폰 완료: " . $this->getName());
     }
 
     public function onUpdate(int $currentTick): bool {
-        $this->getLogger()->info("좀비 업데이트 중: " . $this->getName());
-        // AI 로직을 이곳에 넣어줍니다.
-        $this->plugin->getScheduler()->scheduleRepeatingTask(new MobAITask($this->plugin), 20);
+        $this->getServer()->getLogger()->info("좀비 업데이트 중: " . $this->getName());
         return parent::onUpdate($currentTick);
     }
 }
