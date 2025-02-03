b5e048282b71c484dc39a9d5d750031bd0764ba6
jun04 <jun040329@gmail.com>
2025-02-03 19:47:04 +0900

    Update Zombie.php

--- HybridAI/src/HybridMobAI/Zombie.php
+++ HybridAI/src/HybridMobAI/Zombie.php
@@ -6,6 +6,7 @@ use pocketmine\entity\Living;
 use pocketmine\nbt\tag\CompoundTag;
 use pocketmine\world\World;
 use pocketmine\entity\Location;
+use pocketmine\entity\EntitySizeInfo;
 
 class Zombie extends Living {
 
@@ -21,5 +22,17 @@ class Zombie extends Living {
     public function onUpdate(int $currentTick): bool {
         $this->getServer()->getLogger()->info("좀비 업데이트 중: " . $this->getName());
         return parent::onUpdate($currentTick);
+    }
+
+    public function getName(): string {
+        return "Zombie";
+    }
+
+    protected function getInitialSizeInfo(): EntitySizeInfo {
+        return new EntitySizeInfo(1.8, 0.6); // 높이와 너비를 설정
+    }
+
+    public static function getNetworkTypeId(): string {
+        return "minecraft:zombie";
     }
 }
