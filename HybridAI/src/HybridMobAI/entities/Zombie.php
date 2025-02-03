ab356c383b1427f43fd9ae306be3eab805dafa7d
jun04 <jun040329@gmail.com>
2025-02-03 19:21:37 +0900

    Update Zombie.php

--- HybridAI/src/HybridMobAI/entities/Zombie.php
+++ HybridAI/src/HybridMobAI/entities/Zombie.php
@@ -1,15 +1,25 @@
-<?php namespace HybridMobAI\entities; use pocketmine\entity\Monster; 
-use pocketmine\nbt\tag\CompoundTag; use pocketmine\world\World; use 
-pocketmine\entity\Location; class Zombie extends Monster {
-    public function __construct(Location $location, CompoundTag $nbt) { 
+<?php
+
+namespace HybridMobAI\entities;
+
+use pocketmine\entity\Monster;
+use pocketmine\nbt\tag\CompoundTag;
+use pocketmine\world\World;
+use pocketmine\entity\Location;
+
+class Zombie extends Monster {
+
+    public function __construct(Location $location, CompoundTag $nbt) {
         parent::__construct($location, $nbt);
     }
-    public function onSpawn(): void { parent::onSpawn(); 
-        $this->server->getLogger()->info("좀비 스폰 완료: " . 
-        $this->getName());
+
+    public function onSpawn(): void {
+        parent::onSpawn();
+        $this->server->getLogger()->info("좀비 스폰 완료: " . $this->getName());
     }
-    public function onUpdate(int $currentTick): bool { 
-        $this->server->getLogger()->info("좀비 업데이트 중: " . 
-        $this->getName()); return parent::onUpdate($currentTick);
+
+    public function onUpdate(int $currentTick): bool {
+        $this->server->getLogger()->info("좀비 업데이트 중: " . $this->getName());
+        return parent::onUpdate($currentTick);
     }
 }
