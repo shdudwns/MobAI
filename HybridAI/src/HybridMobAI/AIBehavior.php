ae3b227f3022f9c0dfaa96890f1253a978a78307
jun04 <jun040329@gmail.com>
2025-02-05 01:04:04 +0900

    Update AIBehavior.php

--- HybridAI/src/HybridMobAI/AIBehavior.php
+++ HybridAI/src/HybridMobAI/AIBehavior.php
@@ -2,18 +2,17 @@
 
 namespace HybridMobAI;
 
-use pocketmine\entity\Creature;
+use pocketmine\entity\Monster;
 use pocketmine\math\Vector3;
 
 class AIBehavior {
-
     private $plugin;
 
     public function __construct($plugin) {
         $this->plugin = $plugin;
     }
 
-    public function moveRandomly(Creature $mob): void {
+    public function moveRandomly(Monster $mob): void {
         $this->plugin->getLogger()->info("랜덤 이동 중: " . $mob->getName());
         $directionVectors = [
             new Vector3(1, 0, 0),
@@ -26,22 +25,22 @@ class AIBehavior {
         $mob->setRotation(mt_rand(0, 360), mt_rand(-90, 90));
     }
 
-    public function moveToPlayer(Creature $mob, $player): void {
+    public function moveToPlayer(Monster $mob, $player): void {
         $this->plugin->getLogger()->info("플레이어에게 이동 중: " . $mob->getName());
         // 플레이어에게 이동하는 로직
     }
 
-    public function attackPlayer(Creature $mob, $player): void {
+    public function attackPlayer(Monster $mob, $player): void {
         $this->plugin->getLogger()->info("플레이어 공격 중: " . $mob->getName());
         // 플레이어를 공격하는 로직
     }
 
-    public function retreat(Creature $mob): void {
+    public function retreat(Monster $mob): void {
         $this->plugin->getLogger()->info("후퇴 중: " . $mob->getName());
         // 후퇴하는 로직
     }
 
-    public function jump(Creature $mob): void {
+    public function jump(Monster $mob): void {
         $this->plugin->getLogger()->info("점프 중: " . $mob->getName());
         $jumpForce = 0.5;
         $mob->setMotion(new Vector3($mob->getMotion()->getX(), $jumpForce, $mob->getMotion()->getZ()));
