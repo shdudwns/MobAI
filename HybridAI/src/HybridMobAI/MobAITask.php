f7fa54440de77b91b5733b4beb217abe19fae345
jun04 <jun040329@gmail.com>
2025-02-06 11:18:30 +0900

    Update MobAITask.php

--- HybridAI/src/HybridMobAI/MobAITask.php
+++ HybridAI/src/HybridMobAI/MobAITask.php
@@ -66,28 +66,33 @@ class MobAITask extends Task {
     }
 
     private function moveToPlayer(Zombie $mob, Player $player): void {
-    $mobPos = $mob->getPosition();
-    $playerPos = $player->getPosition(); // 이미 Position 객체
+        $mobPos = $mob->getPosition();
+        $playerPos = $player->getPosition();
 
-    $playerVec3 = new Vector3($playerPos->getX(), $playerPos->getY(), $playerPos->getZ());
+        // Position 객체를 Vector3 객체로 변환
+        $mobVec3 = new Vector3($mobPos->getX(), $mobPos->getY(), $mobPos->getZ());
+        $playerVec3 = new Vector3($playerPos->getX(), $playerPos->getY(), $playerPos->getZ());
 
-    $distance = $mobPos->distance($playerVec3);
-    $speed = 0.2;
-    if ($distance < 5) {
-        $speed *= $distance / 5;
+        $distance = $mobVec3->distance($playerVec3); // 이제 Vector3 객체로 연산
+        $speed = 0.2;
+        if ($distance < 5) {
+            $speed *= $distance / 5;
+        }
+
+        $motion = $playerVec3->subtract($mobVec3)->normalize()->multiply($speed); // 수정된 부분
+        $mob->setMotion($motion);
+        $mob->lookAt($playerPos);
     }
-
-    $motion = $playerVec3->subtract($mobPos)->normalize()->multiply($speed);
-    $mob->setMotion($motion);
-    $mob->lookAt($playerPos); // 수정 없음. 이미 Position 객체
-}
-
 
     private function checkForObstaclesAndJump(Living $mob): void {
         $position = $mob->getPosition();
         $world = $mob->getWorld();
         $yaw = $mob->getLocation()->getYaw();
         $direction2D = VectorMath::getDirection2D($yaw);
+
+        // Position 객체를 Vector3 객체로 변환
+        $position = new Vector3($position->getX(), $position->getY(), $position->getZ());
+
         $directionVector = new Vector3($direction2D->getX(), 0, $direction2D->getY());
 
         $basePosition = new Vector3($position->getX(), $position->getY() + 0.1, $position->getZ());
