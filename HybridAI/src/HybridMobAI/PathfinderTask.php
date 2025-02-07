f2be950c72930178e05a4cc80c8422bbaa4ac2a6
jun04 <jun040329@gmail.com>
2025-02-07 16:44:21 +0900

    Update PathfinderTask.php

--- HybridAI/src/HybridMobAI/PathfinderTask.php
+++ HybridAI/src/HybridMobAI/PathfinderTask.php
@@ -6,7 +6,6 @@ use pocketmine\scheduler\AsyncTask;
 use pocketmine\math\Vector3;
 use pocketmine\Server;
 use pocketmine\entity\Creature;
-use pocketmine\player\Player;
 
 class PathfinderTask extends AsyncTask {
     private float $startX, $startY, $startZ;
@@ -54,12 +53,13 @@ class PathfinderTask extends AsyncTask {
         if ($plugin instanceof Main) {
             $mobAITask = $plugin->getMobAITask();
 
+            // 경로를 현재 경로로 저장
             if ($path === null) {
+                // 경로를 찾지 못하면 랜덤 이동 실행
                 $mobAITask->moveRandomly($entity);
             } else {
-                if ($entity instanceof Creature) {
-                    $mobAITask->setPath($this->mobId, $path); // MobAITask에 경로 설정
-                }
+                // 정상적으로 경로를 찾았을 경우 이동
+                $mobAITask->currentPaths[$this->mobId] = $path; // 경로 저장
             }
         }
     }
