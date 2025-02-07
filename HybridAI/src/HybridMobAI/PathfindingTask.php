4856ebd6e3300b07f197e8cca656e871ec3e40db
jun04 <jun040329@gmail.com>
2025-02-07 12:39:01 +0900

    Update PathfindingTask.php

--- HybridAI/src/HybridMobAI/PathfindingTask.php
+++ HybridAI/src/HybridMobAI/PathfindingTask.php
@@ -6,31 +6,39 @@ use pocketmine\scheduler\AsyncTask;
 use pocketmine\math\Vector3;
 use pocketmine\Server;
 use pocketmine\entity\Creature;
+use pocketmine\world\Position;
 
 class PathfindingTask extends AsyncTask {
-    private $start;
-    private $goal;
+    private float $startX, $startY, $startZ; // float 좌표 저장
+    private float $goalX, $goalY, $goalZ;   // float 좌표 저장
     private $grid;
     private $mobId;
     private $algorithm;
-    private $worldName; // 월드 이름 추가
+    private $worldName;
     private $callback;
 
-    public function __construct($start, $goal, $grid, $mobId, $algorithm, $worldName, callable $callback) {
-        $this->start = $start;
-        $this->goal = $goal;
+    public function __construct(Position $start, Position $goal, $grid, $mobId, $algorithm, $worldName, callable $callback) {
+        $this->startX = $start->getX(); // 좌표 추출
+        $this->startY = $start->getY();
+        $this->startZ = $start->getZ();
+        $this->goalX = $goal->getX();   // 좌표 추출
+        $this->goalY = $goal->getY();
+        $this->goalZ = $goal->getZ();
         $this->grid = $grid;
         $this->mobId = $mobId;
         $this->algorithm = $algorithm;
-        $this->worldName = $worldName; // 월드 이름 초기화
+        $this->worldName = $worldName;
         $this->callback = $callback;
     }
 
     public function onRun(): void {
         $pathfinder = new Pathfinder();
-        $path = $pathfinder->findPath($this->start, $this->goal, $this->grid, $this->algorithm);
+        $start = new Vector3($this->startX, $this->startY, $this->startZ); // Vector3 생성
+        $goal = new Vector3($this->goalX, $this->goalY, $this->goalZ);   // Vector3 생성
+        $path = $pathfinder->findPath($start, $goal, $this->grid, $this->algorithm);
         $this->setResult($path);
     }
+
 
     public function onCompletion(): void {
         $server = Server::getInstance();
