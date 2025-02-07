920ebbb1970b907ea4245604f749719df537cd2d
jun04 <jun040329@gmail.com>
2025-02-07 12:31:13 +0900

    Update PathfindingTask.php

--- HybridAI/src/HybridMobAI/PathfindingTask.php
+++ HybridAI/src/HybridMobAI/PathfindingTask.php
@@ -3,24 +3,27 @@
 namespace HybridMobAI;
 
 use pocketmine\scheduler\AsyncTask;
+use pocketmine\math\Vector3;
 use pocketmine\Server;
 use pocketmine\entity\Creature;
-use pocketmine\math\Vector3;
 
 class PathfindingTask extends AsyncTask {
-
     private $start;
     private $goal;
     private $grid;
     private $mobId;
     private $algorithm;
+    private $worldName; // 월드 이름 추가
+    private $callback;
 
-    public function __construct($start, $goal, $grid, $mobId, $algorithm) {
+    public function __construct($start, $goal, $grid, $mobId, $algorithm, $worldName, callable $callback) {
         $this->start = $start;
         $this->goal = $goal;
         $this->grid = $grid;
         $this->mobId = $mobId;
         $this->algorithm = $algorithm;
+        $this->worldName = $worldName; // 월드 이름 초기화
+        $this->callback = $callback;
     }
 
     public function onRun(): void {
@@ -31,29 +34,34 @@ class PathfindingTask extends AsyncTask {
 
     public function onCompletion(): void {
         $server = Server::getInstance();
-        $path = $this->getResult();
-        $mob = $server->getWorldManager()->getWorldById($this->mobId);
-        if ($path === null) {
-            if ($mob instanceof Creature) {
-                $this->moveRandomly($mob); // 랜덤 이동
-            }
-        } else {
-            if ($mob instanceof Creature) {
-                $nextStep = $path[1];
-                $mob->lookAt($nextStep);
-                $mob->move($nextStep->subtractVector($mob->getPosition())->normalize()->multiply(0.25));
-            }
-        }
-    }
+        $world = $server->getWorldManager()->getWorldByName($this->worldName); // 월드 이름으로 월드 객체 가져오기
 
-    private function moveRandomly(Creature $mob): void {
-        $directionVectors = [
-            new Vector3(1, 0, 0),
-            new Vector3(-1, 0, 0),
-            new Vector3(0, 0, 1),
-            new Vector3(0, 0, -1)
-        ];
-        $randomDirection = $directionVectors[array_rand($directionVectors)];
-        $mob->move($randomDirection->multiply(0.25));
+        if (!$world) {
+            $server->getLogger()->warning("World not found: " . $this->worldName);
+            return;
+        }
+
+        $path = $this->getResult();
+        $entity = $world->getEntity($this->mobId);
+
+        if (!($entity instanceof Creature)) { // Creature instance 확인
+            return;
+        }
+
+        if ($path === null) {
+            $callback = $this->callback;
+            $server->getScheduler()->scheduleTask(new \pocketmine\scheduler\Task(function () use ($callback, $entity) {
+                if (is_callable($callback)) {
+                    call_user_func($callback, $entity, null);
+                }
+            }, 0, false));
+        } else {
+            $callback = $this->callback;
+            $server->getScheduler()->scheduleTask(new \pocketmine\scheduler\Task(function () use ($callback, $entity, $path) {
+                if (is_callable($callback)) {
+                    call_user_func($callback, $entity, $path);
+                }
+            }, 0, false));
+        }
     }
 }
