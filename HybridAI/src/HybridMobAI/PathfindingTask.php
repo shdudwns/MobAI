ec2cd9916daf32ec23b7cf41c740c847c7363131
jun04 <jun040329@gmail.com>
2025-02-07 12:46:00 +0900

    Update PathfindingTask.php

--- HybridAI/src/HybridMobAI/PathfindingTask.php
+++ HybridAI/src/HybridMobAI/PathfindingTask.php
@@ -3,14 +3,15 @@
 namespace HybridMobAI;
 
 use pocketmine\scheduler\AsyncTask;
+use pocketmine\scheduler\ClosureTask;
 use pocketmine\math\Vector3;
 use pocketmine\Server;
 use pocketmine\entity\Creature;
 use pocketmine\world\Position;
 
 class PathfindingTask extends AsyncTask {
-    private float $startX, $startY, $startZ; // float 좌표 저장
-    private float $goalX, $goalY, $goalZ;   // float 좌표 저장
+    private float $startX, $startY, $startZ;
+    private float $goalX, $goalY, $goalZ;
     private $grid;
     private $mobId;
     private $algorithm;
@@ -18,10 +19,10 @@ class PathfindingTask extends AsyncTask {
     private $callback;
 
     public function __construct(Position $start, Position $goal, $grid, $mobId, $algorithm, $worldName, callable $callback) {
-        $this->startX = $start->getX(); // 좌표 추출
+        $this->startX = $start->getX();
         $this->startY = $start->getY();
         $this->startZ = $start->getZ();
-        $this->goalX = $goal->getX();   // 좌표 추출
+        $this->goalX = $goal->getX();
         $this->goalY = $goal->getY();
         $this->goalZ = $goal->getZ();
         $this->grid = $grid;
@@ -33,36 +34,38 @@ class PathfindingTask extends AsyncTask {
 
     public function onRun(): void {
         $pathfinder = new Pathfinder();
-        $start = new Vector3($this->startX, $this->startY, $this->startZ); // Vector3 생성
-        $goal = new Vector3($this->goalX, $this->goalY, $this->goalZ);   // Vector3 생성
+        $start = new Vector3($this->startX, $this->startY, $this->startZ);
+        $goal = new Vector3($this->goalX, $this->goalY, $this->goalZ);
         $path = $pathfinder->findPath($start, $goal, $this->grid, $this->algorithm);
         $this->setResult($path);
     }
 
-
     public function onCompletion(): void {
         $server = Server::getInstance();
-        // ... (world and entity retrieval)
+        $world = $server->getWorldManager()->getWorldByName($this->worldName);
+        if ($world === null) {
+            return;
+        }
+        $entity = $world->getEntity($this->mobId);
+        if (!$entity instanceof Creature) {
+            return;
+        }
 
         $path = $this->getResult();
+        $callback = $this->callback;
 
         if ($path === null) {
-            $callback = $this->callback; // 복사!
-            $entity = $entity;          // 복사!
-            $server->getScheduler()->scheduleTask(new \pocketmine\scheduler\Task(function () use ($callback, $entity) {
+            $server->getScheduler()->scheduleTask(new ClosureTask(function () use ($callback, $entity) {
                 if (is_callable($callback)) {
                     call_user_func($callback, $entity, null);
                 }
-            }, 0, false));
+            }));
         } else {
-            $callback = $this->callback; // 복사!
-            $entity = $entity;          // 복사!
-            $path = $path;              // 복사!
-            $server->getScheduler()->scheduleTask(new \pocketmine\scheduler\Task(function () use ($callback, $entity, $path) {
+            $server->getScheduler()->scheduleTask(new ClosureTask(function () use ($callback, $entity, $path) {
                 if (is_callable($callback)) {
                     call_user_func($callback, $entity, $path);
                 }
-            }, 0, false));
+            }));
         }
     }
 }
