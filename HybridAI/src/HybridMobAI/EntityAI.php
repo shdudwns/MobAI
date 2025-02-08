73a156c323ce409818ab07747e1bbd34a65446ba
jun04 <jun040329@gmail.com>
2025-02-08 17:45:01 +0900

    Update EntityAI.php

--- HybridAI/src/HybridMobAI/EntityAI.php
+++ HybridAI/src/HybridMobAI/EntityAI.php
@@ -49,88 +49,54 @@ class EntityAI {
         $capturedGoal = $goal;
         $capturedAlgorithm = $algorithm;
 
-
         // 2. 콜백 함수를 래핑하는 *일반 함수*를 만듭니다. (익명 함수가 아님)
         $callbackFunction = function($result) use ($capturedThis, $capturedWorld, $capturedStart, $capturedGoal, $capturedAlgorithm, $callback) {
             $callback($result, $capturedThis, $capturedWorld, $capturedStart, $capturedGoal, $capturedAlgorithm);
         };
 
-        Server::getInstance()->getAsyncPool()->submitTask(new class($worldName, $startX, $startY, $startZ, $goalX, $goalY, $goalZ, $algorithm, $callbackFunction, $entityAI, $plugin) extends AsyncTask {
-            private string $worldName;
-            private float $startX;
-            private float $startY;
-            private float $startZ;
-            private float $goalX;
-            private float $goalY;
-            private float $goalZ;
-            private string $algorithm;
-            private $callbackFunction; // 일반 함수 저장
-            private EntityAI $entityAI;
-            private PluginBase $plugin;
+        // 3. AsyncTask 객체 생성을 별도의 함수로 분리 (핵심 변경 사항)
+        $createAsyncTask = function() use ($worldName, $startX, $startY, $startZ, $goalX, $goalY, $goalZ, $algorithm, $callbackFunction, $entityAI, $plugin) {
+            return new class($worldName, $startX, $startY, $startZ, $goalX, $goalY, $goalZ, $algorithm, $callbackFunction, $entityAI, $plugin) extends AsyncTask {
+                // ... (AsyncTask 속성들 - 이전과 동일)
 
-            private EntityAI $capturedThis;
-            private World $capturedWorld;
-            private Position $capturedStart;
-            private Position $capturedGoal;
-            private string $capturedAlgorithm;
-
-
-            public function __construct(string $worldName, float $startX, float $startY, float $startZ, float $goalX, float $goalY, float $goalZ, string $algorithm, callable $callbackFunction, EntityAI $entityAI, PluginBase $plugin) {
-                $this->worldName = $worldName;
-                $this->startX = $startX;
-                $this->startY = $startY;
-                $this->startZ = $startZ;
-                $this->goalX = $goalX;
-                $this->goalY = $goalY;
-                $this->goalZ = $goalZ;
-                $this->algorithm = $algorithm;
-                $this->callbackFunction = $callbackFunction;
-                $this->entityAI = $entityAI;
-                $this->plugin = $plugin;
-
-                $this->capturedThis = $capturedThis;
-                $this->capturedWorld = $capturedWorld;
-                $this->capturedStart = $capturedStart;
-                $this->capturedGoal = $capturedGoal;
-                $this->capturedAlgorithm = $capturedAlgorithm;
-            }
-
-            public function onRun(): void {
-                $world = Server::getInstance()->getWorldManager()->getWorldByName($this->worldName);
-                if (!$world instanceof World) {
-                    $this->setResult(null);
-                    return;
+                public function __construct(string $worldName, float $startX, float $startY, float $startZ, float $goalX, float $goalY, float $goalZ, string $algorithm, callable $callbackFunction, EntityAI $entityAI, PluginBase $plugin) {
+                    // ... (AsyncTask 생성자 - 이전과 동일)
+                    $this->callbackFunction = $callbackFunction;
                 }
 
-                $start = new Vector3($this->startX, $this->startY, $this->startZ);
-                $goal = new Vector3($this->goalX, $this->goalY, $this->goalZ);
-                $pathfinder = new Pathfinder();
+                public function onRun(): void {
+                    $world = Server::getInstance()->getWorldManager()->getWorldByName($this->worldName);
+                    if (!$world instanceof World) {
+                        $this->setResult(null);
+                        return;
+                    }
 
-                $path = match ($this->algorithm) {
-                    "A*" => $pathfinder->findPathAStar($world, $start, $goal),
-                    "Dijkstra" => $pathfinder->findPathDijkstra($world, $start, $goal),
-                    "Greedy" => $pathfinder->findPathGreedy($world, $start, $goal),
-                    "BFS" => $pathfinder->findPathBFS($world, $start, $goal),
-                    "DFS" => $pathfinder->findPathDFS($world, $start, $goal),
-                    default => null,
-                };
+                    $start = new Vector3($this->startX, $this->startY, $this->startZ);
+                    $goal = new Vector3($this->goalX, $this->goalY, $this->goalZ);
+                    $pathfinder = new Pathfinder();
 
-                $this->setResult($path);
-            }
+                    $path = match ($this->algorithm) {
+                        "A*" => $pathfinder->findPathAStar($world, $start, $goal),
+                        "Dijkstra" => $pathfinder->findPathDijkstra($world, $start, $goal),
+                        "Greedy" => $pathfinder->findPathGreedy($world, $start, $goal),
+                        "BFS" => $pathfinder->findPathBFS($world, $start, $goal),
+                        "DFS" => $pathfinder->findPathDFS($world, $start, $goal),
+                        default => null,
+                    };
 
-            public function onCompletion(): void {
-                $result = $this->getResult();
-
-                // 3. 래핑된 *일반 함수*를 호출합니다.
-                ($this->callbackFunction)($result);
-
-                if ($result !== null) {
-                    $this->plugin->getLogger()->info("경로 탐색 완료!");
-                } else {
-                    $this->plugin->getLogger()->info("경로를 찾을 수 없습니다.");
+                    $this->setResult($path);
                 }
-            }
-        });
+
+                public function onCompletion(): void {
+                    $result = $this->getResult();
+                    ($this->callbackFunction)($result); // 래핑된 콜백 호출
+                    // ... (로그 출력 - 이전과 동일)
+                }
+            };
+        };
+
+        // 4. AsyncTask 실행
+        Server::getInstance()->getAsyncPool()->submitTask($createAsyncTask()); // 함수 호출 후 반환된 AsyncTask 객체 전달
     }
     public function findPath(World $world, Vector3 $start, Vector3 $goal, string $algorithm): ?array {
         $pathfinder = new Pathfinder();
