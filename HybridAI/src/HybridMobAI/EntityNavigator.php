5881fb7b2f804b0f95c62ca9f6fa6d1814bc113c
jun04 <jun040329@gmail.com>
2025-02-09 01:37:52 +0900

    Update EntityNavigator.php

--- HybridAI/src/HybridMobAI/EntityNavigator.php
+++ HybridAI/src/HybridMobAI/EntityNavigator.php
@@ -10,13 +10,13 @@ class EntityNavigator {
     public function moveToPlayer(Living $mob, Player $player): void {
         $mobPos = $mob->getPosition();
         $playerPos = $player->getPosition();
-
-        $speed = 0.2; // 속도를 일정하게 유지
-
+    
+        $speed = 0.15;
         $motion = $playerPos->subtractVector($mobPos)->normalize()->multiply($speed);
         $currentMotion = $mob->getMotion();
+    
+        $inertiaFactor = 0.6; 
 
-        $inertiaFactor = 0.2; // 부드러운 이동 적용
         $blendedMotion = new Vector3(
             ($currentMotion->x * $inertiaFactor) + ($motion->x * (1 - $inertiaFactor)),
             $currentMotion->y,
@@ -35,5 +35,22 @@ class EntityNavigator {
             $mob->setMotion($nextPosition->subtractVector($mob->getPosition())->normalize()->multiply(0.2));
             $mob->lookAt($nextPosition);
         }
+    }
+
+    public function moveRandomly(Living $mob): void {
+        $directionVectors = [
+            new Vector3(1, 0, 0), new Vector3(-1, 0, 0),
+            new Vector3(0, 0, 1), new Vector3(0, 0, -1)
+        ];
+        $randomDirection = $directionVectors[array_rand($directionVectors)];
+
+        $currentMotion = $mob->getMotion();
+        $blendedMotion = new Vector3(
+            ($currentMotion->x * 0.8) + ($randomDirection->x * 0.2),
+            $currentMotion->y,
+            ($currentMotion->z * 0.8) + ($randomDirection->z * 0.2)
+        );
+
+        $mob->setMotion($blendedMotion);
     }
 }
