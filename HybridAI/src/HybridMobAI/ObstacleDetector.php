f43553cdf8b16ea532486291d0f5d6fde40c0f0b
jun04 <jun040329@gmail.com>
2025-02-09 01:12:09 +0900

    Create ObstacleDetector.php

---
+++ HybridAI/src/HybridMobAI/ObstacleDetector.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace HybridMobAI;
+
+use pocketmine\entity\Living;
+use pocketmine\math\Vector3;
+use pocketmine\world\World;
+use pocketmine\block\Block;
+use pocketmine\block\Stair;
+use pocketmine\block\Slab;
+
+class ObstacleDetector {
+    public function checkForObstaclesAndJump(Living $mob, World $world): void {
+        $position = $mob->getPosition();
+        $yaw = $mob->getLocation()->yaw;
+        $direction2D = VectorMath::getDirection2D($yaw);
+        $directionVector = new Vector3($direction2D->x, 0, $direction2D->y);
+
+        $frontBlockPos = $position->addVector($directionVector);
+        $frontBlock = $world->getBlockAt((int)$frontBlockPos->x, (int)$frontBlockPos->y, (int)$frontBlockPos->z);
+        $frontBlockAbove = $world->getBlockAt((int)$frontBlockPos->x, (int)$frontBlockPos->y + 1, (int)$frontBlockPos->z);
+        
+        $heightDiff = $frontBlock->getPosition()->y + 1 - $position->y;
+
+        if ($heightDiff < 0.3) return;
+
+        if ($frontBlock instanceof Stair || $frontBlock instanceof Slab) {
+            $this->stepUp($mob, $heightDiff);
+        } else if ($heightDiff <= 1.5) {
+            $this->jump($mob, $heightDiff);
+        }
+    }
+
+    private function stepUp(Living $mob, float $heightDiff): void {
+        $mob->setMotion(new Vector3($mob->getMotion()->x, 0.5, $mob->getMotion()->z));
+    }
+
+    private function jump(Living $mob, float $heightDiff): void {
+        $mob->setMotion(new Vector3($mob->getMotion()->x, 0.7, $mob->getMotion()->z));
+    }
+}
